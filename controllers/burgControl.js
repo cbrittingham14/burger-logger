5ddbb8e231952edc3f5130d7cbd8008053a5317a
chizz <cbrittingham@gmail.com>
2020-03-26 16:14:27 -0500

    added basic route

--- controllers/burgControl.js
+++ controllers/burgControl.js
@@ -3,3 +3,8 @@ const express = require("express");
 var router = express.Router();
 
 module.exports = router;
+
+router.get("/", function(req,res){
+    console.log("it worked");
+    res.end();
+});
