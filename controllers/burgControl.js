56db506f9c5e666c1e7d1104a0638689010c137a
chizz <cbrittingham@gmail.com>
2020-03-26 17:24:14 -0500

    added route to read id from ajax request

--- controllers/burgControl.js
+++ controllers/burgControl.js
@@ -3,8 +3,6 @@ const burg = require("../models/burger.js");
 
 var router = express.Router();
 
-module.exports = router;
-
 router.get("/", function(req,res){
     console.log("it worked");
     burg.getAll(data =>{
@@ -15,3 +13,12 @@ router.get("/", function(req,res){
         res.render("index", burgObj);
     });
 });
+
+router.get("/api/devour/:id", function( req, res){
+    const id = req.params.id;
+    console.log("id in /api/devour ", id);
+    res.json("we responded");
+})
+
+
+module.exports = router;
