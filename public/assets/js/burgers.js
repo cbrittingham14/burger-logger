fec1ec06eca401242b76cfb2d70fb1719698290e
chizz <cbrittingham@gmail.com>
2020-03-27 14:33:13 -0500

    made page reload after ajax request compeletes

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -3,12 +3,12 @@ $(function(){
     $(".devour").on("click", function(e){
         e.preventDefault();
         const id = $(this).data("id");
-        console.log("you clicked id: ", id);
 
         $.ajax("/api/devour/" + id, {
             type: "GET"
         }).then( function(){
             console.log("sent a request");
+            location.reload();
         });
     });
 
@@ -17,11 +17,12 @@ $(function(){
         const b ={
             burg: $("#bu").val().trim()
         };
-        console.log(b);
-
         $.ajax("/api/devour", {
             type: "POST",
             data: b
-        }).then(()=> console.log("added a burger"));
+        }).then(()=> {
+            console.log("added a burger");
+            location.reload();
+        });
     })
 });
